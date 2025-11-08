9f2791af08c9191fbdd8f3cba3f8c8e4564568d9
psatomas <psatomas@gmail.com>
2025-11-08 18:21:16 -0300

    feat: add Museum struct and revenue test

---
+++ target/debug/deps/writing_a_test_and_the_assert_eq_macro-337548fffa10e5ee.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/testing/target/debug/deps/writing_a_test_and_the_assert_eq_macro-337548fffa10e5ee.d: src/bin/writing_a_test_and_the_assert_eq_macro.rs
+
+/home/tomas/Documents/projects/rust/testing/target/debug/deps/libwriting_a_test_and_the_assert_eq_macro-337548fffa10e5ee.rmeta: src/bin/writing_a_test_and_the_assert_eq_macro.rs
+
+src/bin/writing_a_test_and_the_assert_eq_macro.rs:
---
+++ target/debug/deps/writing_a_test_and_the_assert_eq_macro-a8489ce322016fb2.d
@@ -0,0 +1,5 @@
+/home/tomas/Documents/projects/rust/testing/target/debug/deps/writing_a_test_and_the_assert_eq_macro-a8489ce322016fb2.d: src/bin/writing_a_test_and_the_assert_eq_macro.rs
+
+/home/tomas/Documents/projects/rust/testing/target/debug/deps/libwriting_a_test_and_the_assert_eq_macro-a8489ce322016fb2.rmeta: src/bin/writing_a_test_and_the_assert_eq_macro.rs
+
+src/bin/writing_a_test_and_the_assert_eq_macro.rs:
